12107e48b863f2a2495b44523cbd452c4e747f09
Matilda Mared <matildamared@live.se>
2023-08-04 14:56:12 +0200

    work on lecture 01

--- lecture-01/hello.c
+++ lecture-01/hello.c
@@ -1,5 +1,8 @@
+#include <cs50.h>
 #include <stdio.h>
 
-int main(void){
-  printf("Hello, world!\n");
+int main(void)
+{
+  string answer = get_string("What's your name?");
+  printf("Hello, %s!\n", answer);
 }
